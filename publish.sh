463cd5b03ba2356921862f9d3abaf5fddaddeab5
Sam Gross <colesbury@gmail.com>
2020-05-12 12:02:22 -0400

    Add script to copy packages to nogil org

---
+++ publish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Use `conda search --override-channels -c colesbury` to find
+# packages.
+anaconda copy colesbury/apipkg/1.5 --to-owner nogil
+anaconda copy colesbury/certifi/2020.4.5.1 --to-owner nogil
+anaconda copy colesbury/cython/0.29.17 --to-owner nogil
+anaconda copy colesbury/h5py/2.10.0 --to-owner nogil
+anaconda copy colesbury/magma-cuda101/2.5.2 --to-owner nogil
+anaconda copy colesbury/mkl-service/2.3.0 --to-owner nogil
+anaconda copy colesbury/mkl_fft/1.0.15 --to-owner nogil
+anaconda copy colesbury/mkl_random/1.1.0 --to-owner nogil
+anaconda copy colesbury/mpmath/1.1.0 --to-owner nogil
+anaconda copy colesbury/nose/1.3.7 --to-owner nogil
+anaconda copy colesbury/numpy/1.14.6 --to-owner nogil
+anaconda copy colesbury/numpy/1.14.6 --to-owner nogil
+anaconda copy colesbury/numpy/1.18.1 --to-owner nogil
+anaconda copy colesbury/numpy/1.18.1 --to-owner nogil
+anaconda copy colesbury/numpy-base/1.14.6 --to-owner nogil
+anaconda copy colesbury/numpy-base/1.18.1 --to-owner nogil
+anaconda copy colesbury/numpy-base/1.18.1 --to-owner nogil
+anaconda copy colesbury/numpy-devel/1.14.6 --to-owner nogil
+anaconda copy colesbury/numpy-devel/1.18.1 --to-owner nogil
+anaconda copy colesbury/numpy-devel/1.18.1 --to-owner nogil
+anaconda copy colesbury/pillow/7.1.2 --to-owner nogil
+anaconda copy colesbury/pip/20.1 --to-owner nogil
+anaconda copy colesbury/pkgconfig/1.4.0 --to-owner nogil
+anaconda copy colesbury/pluggy/0.13.1 --to-owner nogil
+anaconda copy colesbury/pybind11/2.4.3 --to-owner nogil
+anaconda copy colesbury/pytest/5.4.1 --to-owner nogil
+anaconda copy colesbury/python/3.9.0 --to-owner nogil
+anaconda copy colesbury/pytorch/1.4.1 --to-owner nogil
+anaconda copy colesbury/pyyaml/5.3.1 --to-owner nogil
+anaconda copy colesbury/pyzmq/18.1.0 --to-owner nogil
+anaconda copy colesbury/scikit-learn/0.22.1 --to-owner nogil
+anaconda copy colesbury/scikit-learn/0.22.1 --to-owner nogil
+anaconda copy colesbury/scipy/1.4.1 --to-owner nogil
+anaconda copy colesbury/scipy/1.4.1 --to-owner nogil
+anaconda copy colesbury/sentencepiece/0.1.85 --to-owner nogil
+anaconda copy colesbury/setuptools/46.1.3 --to-owner nogil
+anaconda copy colesbury/six/1.14.0 --to-owner nogil
+anaconda copy colesbury/tbb/2020.0 --to-owner nogil
+anaconda copy colesbury/tbb-devel/2020.0 --to-owner nogil
+anaconda copy colesbury/tbb4py/2020.0 --to-owner nogil
+anaconda copy colesbury/tokenizers/0.4.2 --to-owner nogil
+anaconda copy colesbury/torchvision/0.5.0.dev20200511 --to-owner nogil
+anaconda copy colesbury/wheel/0.34.2 --to-owner nogil
