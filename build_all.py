a5161429f283c5d2534ca8764e8ca051a5243387
Sam Gross <colesbury@gmail.com>
2020-10-20 23:38:30 +0000

    Add start of build_all.py script

---
+++ build_all.py
@@ -0,0 +1,47 @@
+import yaml
+
+packages = [
+    apipkg,
+    av,
+    certifi,
+    cffi,
+    cython,
+    h5py,
+    mkl-service,
+    mkl_fft,
+    mkl_random,
+    mpmath,
+    nose,
+    numpy-1.14,
+    numpy,
+    numpy-metapackage,
+    opencv,
+    pandas,
+    pillow,
+    pip,
+    pkgconfig,
+    pluggy,
+    pybind11,
+    pytest,
+    python,
+    pyyaml,
+    pyzmq,
+    scikit-learn,
+    scipy,
+    sentencepiece,
+    setuptools,
+    six,
+    tbb,
+    tokenizers,
+    wheel,
+]
+
+deps = {}
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
