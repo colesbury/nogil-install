95a91490bc6ebd252e0c8ed0cc526b6476e1428f
Sam Gross <colesbury@gmail.com>
2020-10-20 21:00:44 -0400

    wip

--- build_all.py
+++ build_all.py
@@ -163,7 +163,7 @@ def main():
 
     if os.path.exists(args.skip):
         with open(args.skip, 'r') as f:
-            for line in f.readliens():
+            for line in f.readlines():
                 skip.add(line)
         print(f"skipping: {' '.join(skip)}", file=sys.stderr)
 
