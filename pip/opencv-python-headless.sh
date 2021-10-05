ccea46a2e7ce3b812aa8ef4e4ed271059c14a90a
Sam Gross <colesbury@gmail.com>
2021-10-05 16:25:41 -0700

    Add opencv packages

---
+++ pip/opencv-python-headless.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+package=opencv-python-headless
+version=4.5.3.56
+yum_packages=
+
+source build-generic.sh
---
+++ pip/opencv-python.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+package=opencv-python
+version=4.5.3.56
+yum_packages=
+
+source build-generic.sh
