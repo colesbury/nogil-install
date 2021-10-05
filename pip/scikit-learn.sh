9686087dfb7c32e977808e4492b2eaf3165b3590
Sam Gross <colesbury@gmail.com>
2021-10-05 14:24:48 -0700

    Use build-generic.sh for scikit-learn

--- pip/scikit-learn.sh
+++ pip/scikit-learn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-mkdir -p builds
 
-docker run -v `pwd`:/io nogil/numpy /io/build-scikit-learn-wheel.sh
-./upload_wheel.sh wheelhouse/scikit_learn-1.0-nogil39-nogil_39_x86_64_linux_gnu-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+package=scikit-learn
+version=1.0
+yum_packages=
+
+source build-generic.sh
