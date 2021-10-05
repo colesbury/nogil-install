7067207ba0fefd9e33af0c71b04c3bfa475399ff
Sam Gross <colesbury@gmail.com>
2021-10-05 13:04:29 -0700

    Update scikit-learn to 1.0

--- pip/build-scikit-learn-wheel.sh
+++ pip/build-scikit-learn-wheel.sh
@@ -6,7 +6,8 @@ source $MY_DIR/build-common.sh
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" wheel /io/builds/scikit-learn -w /io/wheelhouse/
+    "${PYBIN}/pip" download "scikit-learn==1.0"
+    "${PYBIN}/pip" wheel scikit-learn-1.0.tar.gz -w /io/wheelhouse/
 done
 
-repair_wheel /io/wheelhouse/scikit_learn-0.24.2-nogil39-nogil_39_x86_64_linux_gnu-linux_x86_64.whl
+repair_wheel /io/wheelhouse/scikit_learn-1.0-nogil39-nogil_39_x86_64_linux_gnu-linux_x86_64.whl
--- pip/scikit-learn.sh
+++ pip/scikit-learn.sh
@@ -2,9 +2,5 @@
 set -e
 mkdir -p builds
 
-if [ ! -e builds/scikit-learn ]; then
-  git clone --recurse-submodules https://github.com/colesbury/scikit-learn -b 0.24.2 builds/scikit-learn
-fi
-
 docker run -v `pwd`:/io nogil/numpy /io/build-scikit-learn-wheel.sh
-# ./upload_wheel.sh wheelhouse/scikit-learn-0.24.2-nogil_39_x86_64_linux_gnu-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+./upload_wheel.sh wheelhouse/scikit_learn-1.0-nogil39-nogil_39_x86_64_linux_gnu-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
