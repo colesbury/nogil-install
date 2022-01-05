1b2bce7fc088f92c755476b4084fe1f053966757
Sam Gross <colesbury@gmail.com>
2022-01-05 11:14:14 -0800

    Update script to build CUDA manylinux images

--- pip/pytorch/build_docker.sh
+++ pip/pytorch/build_docker.sh
@@ -1,4 +1,20 @@
 #/bin/bash
 set -e
 
-docker build --rm -t nogil/pytorch .
+SOABI="nogil39-nogil_39b_x86_64_linux_gnu"
+PYTHON_VERSION=3.9.9
+
+function build_image {
+    CUDA_IMAGE="$1"
+    docker build --rm \
+        -t "nogil/${CUDA_IMAGE}" \
+        --build-arg CUDA_IMAGE="${CUDA_IMAGE}" \
+        --build-arg PYTHON_VERSION="${PYTHON_VERSION}" \
+        --build-arg SOABI="${SOABI}" \
+        .
+    docker push "nogil/${CUDA_IMAGE}"
+}
+
+build_image manylinux-cuda102
+build_image manylinux-cuda110
+build_image manylinux-cuda113
