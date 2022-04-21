a869a8e5febdb7a97f95ec535af529af1297c967
Sam Gross <colesbury@gmail.com>
2022-04-20 18:02:11 -0700

    cryptography: use forked pyo3 lib

--- pip/cryptography.sh
+++ pip/cryptography.sh
@@ -3,7 +3,9 @@ set -e
 
 package=cryptography
 version=36.0.2
+url="git+https://github.com/colesbury/cryptography.git@${version}-nogil"
 yum_packages="cargo libffi-devel"
 preinstall_file="preinstall-openssl.sh"
+pip_packages="setuptools-rust"  # setup.py sdist needs setuptools-rust
 
 source build-generic.sh
