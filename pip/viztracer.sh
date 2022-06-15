e2663b957f0685e36278d98fa640a39064c9a085
Sam Gross <colesbury@gmail.com>
2022-06-15 10:40:39 -0700

    Add viztracer

---
+++ pip/viztracer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+package=viztracer
+version=0.15.2
+url="git+https://github.com/gaogaotiantian/viztracer@276e8b731281c8cdf23c5ba09fb4e79a8dc4973d"
+yum_packages=
+
+source build-generic.sh
